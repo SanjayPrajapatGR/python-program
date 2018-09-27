db58e53bb2d7dfc21e1b3bb59cb7293097029d9e
ravipatel73842 <41752089+ravipatel73842@users.noreply.github.com>
2018-09-27 06:35:37 -0700

    Update emirp_number.py

--- emirp_number.py
+++ emirp_number.py
@@ -5,11 +5,11 @@ for i in range(1,number+1,1):
         count=count+1
 if(count == 2):
     count1=0
-    y=str(number)                         "convert number into string
-    z=y[::-1]                             " revese the string
-    z=int(z)                              " convert  reverse string into intger
+    y=str(number)                         "convert number into string"
+    z=y[::-1]                             " revese the string"
+    z=int(z)                              " convert  reverse string into intger"
     for i in range(1,z+1,1):
-        if(z%i == 0):                     " check reverse number for prime
+        if(z%i == 0):                     " check reverse number for prime"
             count1=count1+1
     if(count1 == 2):
         print("the number is emirp")
